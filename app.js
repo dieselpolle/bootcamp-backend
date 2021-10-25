0301fc5fb331dfe9c873494e6d6087021c20124f
Mikko <mikko@markkaset.com>
2021-10-25 15:47:37 +0300

    Modified app.js and set new authentication for user only

--- app.js
+++ app.js
@@ -10,27 +10,8 @@ const cors = require('cors');
 app.use(helmet());
 app.use(cors());
 
-//
-// Enabling basic authentication and environment variables
-//
-const basicAuth = require('express-basic-auth');
-app.use(basicAuth( { authorizer: authAPI, authorizeAsync:true, } ))
-const dotenv = require('dotenv');
-dotenv.config();
-
-//
-//username and password are needed when using REST API, uses basic authentication
-//
-function authAPI(username, password, cb){
-    if(username===process.env.auth_user && password ===process.env.auth_pass){
-        return cb(null, true);
-    }
-    else{
-        return cb(null, false);
-    }
-}
-
 const userRouter = require('./routes/user');
+const userRequestRouter = require('./routes/userRequest');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,5 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', userRouter);
+app.use('/userRequest', userRequestRouter);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router = express.Router();
 const user = require('../models/user_model');
 
+//
+// Enabling basic authentication and environment variables
+//
+const basicAuth = require('express-basic-auth');
+app.use(basicAuth( { authorizer: authAPI, authorizeAsync:true, } ))
+const dotenv = require('dotenv');
+dotenv.config();
+
+//
+//username and password are needed when using REST API, uses basic authentication
+//
+function authAPI(username, password, cb){
+    if(username===process.env.auth_user && password ===process.env.auth_pass){
+        return cb(null, true);
+    }
+    else{
+        return cb(null, false);
+    }
+}
+
 router.get('/:id?',
  function(request, response) {
   if (request.params.id) {
