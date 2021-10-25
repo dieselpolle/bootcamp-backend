0853b28b9bbd6caab675372bcfddfae7d4ffdc5e
Mikko <mikko@markkaset.com>
2021-10-25 16:07:18 +0300

    Some bug fixes

--- models/userRequest_model.js
+++ models/userRequest_model.js
@@ -1,7 +1,7 @@
 const db = require('../database');
 
 const userRequest = {
-  add: function(user, callback) {
+  add: function(userRequest, callback) {
     return db.query(
       'insert into user (username,password,email,allowed) values(?,?,?,?)',
       [user.email, null, user.email,null],
--- routes/userRequest.js
+++ routes/userRequest.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/userRequest_model');
+const userRequest = require('../models/userRequest_model');
 
 router.post('/', 
 function(request, response) {
-  user.add(request.body, function(err, dbResult) {
+  userRequest.add(request.body, function(err, dbResult) {
     if (err) {
       response.json(err);
     } else {
