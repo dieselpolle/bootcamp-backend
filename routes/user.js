3a669705c134d18d63d66ae2c4456c4eb3fa27ec
Mikko <mikko@markkaset.com>
2021-10-26 09:58:50 +0300

    Authentication added

--- routes/user.js
+++ routes/user.js
@@ -59,4 +59,15 @@ function(request, response) {
   });
 });
 
+router.get('/auth',
+function authenticateUser(request, response) {
+  user.authenticateUser(request.params.username, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+});
+});
+
 module.exports = router;
